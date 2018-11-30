0898231b26bc4a5f12ea2de7b565fc65599b9ed9
varley44 <javarley44@gmail.com>
2018-11-30 17:24:11 -0500

    started .js

---
+++ Html docs/Loginscript.js
@@ -0,0 +1,13 @@
+function login(){
+	var user = document.getElementById("user").value;
+	var password = document.getElementById("password").value;
+	
+	data = {
+		"user":user, "password":password
+		}
+	window.alert("HELLO");
+	window.location.replace("homepage.html");
+	if(user != null && user.hasOwnProperty('uid') && user.hasOwnProperty('idToken')){
+		window.location.replace("homepage.html");
+		return;
+	}
