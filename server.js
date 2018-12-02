fd39b5e2d9b2de6c4fe08ab33e5279a22a3f5a46
lkristie <lauragkristie96@gmail.com>
2018-12-01 23:46:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express= require('express');
 var app=express();
 const bodyParser= require("body-parser");
 var fs=require('fs');
-
+//connect firebase with the server / node.js
+//let serviceKey = require('./firebase-config/firebase.service-key.json'); // The service key of the firebase project
+//let firebaseConfig = require('./firebase-config/firebase.config.json'); 
+var admin = require('firebase-admin');
+admin.initializeApp({
+   credential: admin.credential.cert(serviceKey),
+   databaseURL: 'https://event-planner-2bed5.firebaseio.com/'
+ });
  var database=admin.database();
 //respond with the login page
 app.get('/', function (req, res) {
